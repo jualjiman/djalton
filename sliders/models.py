9e8735cc39aad6df4457337ed14b10fe8f8fe459
Juan Alberto Jimenez Angel <jualjiman@gmail.com>
2014-05-04 20:41:21 -0500

    Creados los modelos de sliders

--- sliders/models.py
+++ sliders/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Slider(models.Model):
 	title = models.CharField(max_length=160)
 	image = models.FileField(upload_to = "sliders")
-	
 
 class Label(models.Model):
 	title = models.CharField(max_length=50)
+	slider = models.ForeignKey(Slider)
