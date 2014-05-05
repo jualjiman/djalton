d94659ba520df50e8259fd235672d9fa52993c45
Juan Alberto Jimenez Angel <jualjiman@gmail.com>
2014-05-04 21:00:42 -0500

    Corrigiendo detalles de sliders

--- sliders/models.py
+++ sliders/models.py
@@ -3,8 +3,14 @@ from django.db import models
 # Create your models here.
 class Slider(models.Model):
 	title = models.CharField(max_length=160)
-	image = models.FileField(upload_to = "sliders")
+	image = models.FileField(upload_to = "sliders/assets")
+
+	def __str__(self):
+		return self.title
 
 class Label(models.Model):
 	title = models.CharField(max_length=50)
 	slider = models.ForeignKey(Slider)
+
+	def __str__(self):
+		return self.title
