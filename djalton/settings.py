9cd41b26c469662d7b60641083bb670d233bcd33
jualjiman <jualjiman@gmail.com>
2014-06-26 18:06:25 -0500

    nosotros.html

--- djalton/settings.py
+++ djalton/settings.py
@@ -43,10 +43,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'administrador',
-    'sorl.thumbnail',
     'south',
+    'sorl.thumbnail',
     'columns',
+    'administrador',
 )
 
 MIDDLEWARE_CLASSES = (
