a1bbd62bdc662bffbefa93f4536ae534c6893acb
jualjiman <jualjiman@gmail.com>
2014-06-27 23:46:14 -0500

    Agregando funcionalidad al formulario de contacto

--- administrador/forms.py
+++ administrador/forms.py
@@ -4,4 +4,4 @@ class ContactForm(forms.Form):
     name = forms.CharField(max_length=100,widget=forms.TextInput(attrs={'placeholder': 'Nombre'}))
     email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Email'}))
     phonenumber = forms.CharField(max_length=20,widget=forms.TextInput(attrs={'placeholder': 'Numero telefonico'}))
-    message = forms.CharField(widget=forms.Textarea,widget=forms.Textarea(attrs={'placeholder': 'Mensaje'}))
+    message = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Mensaje'}))
