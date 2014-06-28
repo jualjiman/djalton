354d181c136357c970db50272b7b5be749a9daea
jualjiman <jualjiman@gmail.com>
2014-06-28 12:40:51 -0500

    modulo contacto terminado

--- djalton/settings.py
+++ djalton/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'x)4nmd979l7-16z*3ro4+y*=((_yc50=v&tvbt5al-aw(n^oov'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = True
 
