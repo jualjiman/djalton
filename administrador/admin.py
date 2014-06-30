82c130dbab46ac11d2ae3704809bccdf33f405d5
jualjiman <jualjiman@gmail.com>
2014-06-29 19:08:23 -0500

    redistribuyendo listas de administrador

--- administrador/admin.py
+++ administrador/admin.py
@@ -1,8 +1,42 @@
 from django.contrib import admin
 from .models import *
+from sorl.thumbnail.shortcuts import get_thumbnail
 
 # Register your models here.
-admin.site.register(Slider)
-admin.site.register(Trabajo)
-admin.site.register(Oferta)
-admin.site.register(Empleado)
+
+class SliderAdmin(admin.ModelAdmin):
+	list_display = ('titulo', 'imagen_proyecto_slider')
+
+	def imagen_proyecto_slider(self,obj):
+		return '<img src="%s" />' % get_thumbnail(obj.imagen,'100x60', crop='center').url #format='PNG', quality=99
+
+	imagen_proyecto_slider.allow_tags = True
+
+class TrabajoAdmin(admin.ModelAdmin):
+	list_display = ('titulo','descripcion' 'imagen_proyecto_trabajo')
+
+	def imagen_proyecto_trabajo(self,obj):
+		return '<img src="%s" />' % get_thumbnail(obj.imagen,'100x60', crop='center').url #format='PNG', quality=99
+
+	imagen_proyecto_trabajo.allow_tags = True
+
+class OfertaAdmin(admin.ModelAdmin):
+	list_display = ('titulo', 'descripcion','precio', 'imagen_proyecto_oferta')
+
+	def imagen_proyecto_oferta(self,obj):
+		return '<img src="%s" />' % get_thumbnail(obj.imagen,'100x60', crop='center').url #format='PNG', quality=99
+
+	imagen_proyecto_oferta.allow_tags = True
+
+class EmpleadoAdmin(admin.ModelAdmin):
+	list_display = ('nombre','cargo', 'imagen_proyecto_empleado')
+
+	def imagen_proyecto_empleado(self,obj):
+		return '<img src="%s" />' % get_thumbnail(obj.imagen,'100x60', crop='center').url #format='PNG', quality=99
+
+	imagen_proyecto_empleado.allow_tags = True
+
+admin.site.register(Slider,SliderAdmin)
+admin.site.register(Trabajo,TrabajoAdmin)
+admin.site.register(Oferta,OfertaAdmin)
+admin.site.register(Empleado, EmpleadoAdmin)
--- administrador/views.py
+++ administrador/views.py
@@ -37,36 +37,11 @@ def portafolio(request):
 def portafolioIndividual(request,id):
 	ofertas = Oferta.objects.all()[:5]
 	trabajo = Trabajo.objects.get(pk=id)
-	count = Trabajo.objects.all().count()
+	
+	count = Trabajo.objects.all()
 	rand_ids = sample(xrange(1, count), 2)
 	trabajos = Trabajo.objects.filter(id__in=rand_ids)
 	return render(request,"portafolio_single.html",{"trabajo":trabajo,"trabajos":trabajos,"ofertas":ofertas})
-
-#SIN AJAX
-# def contacto(request):
-# 	if request.method == 'POST':
-# 		form = ContactForm(request.POST)
-# 		if form.is_valid():
-# 		    name = form.cleaned_data['name']
-# 		    email = form.cleaned_data['email']
-# 		    phonenumber = form.cleaned_data['phonenumber']
-# 		    message = form.cleaned_data['message']
-
-# 		    subject = "Mensaje desde el sitio Daltonautomotriz.com"
-
-# 		    fmessage = "Nombre: " + name + "\n" + "Numero telefonico: " + phonenumber + "\n" + "Email: " + email + "\n\n" + message
-
-# 		    recipients = ['jualjiman@gmail.com']
-
-# 		    send_mail(subject, fmessage, email, recipients)
-# 		    return HttpResponseRedirect('/thanks/')
-# 		else:
-# 			ofertas = Oferta.objects.all()[:5]
-# 			return render(request,"contacto.html",{"ofertas":ofertas,"form": form})
-# 	else:
-# 		ofertas = Oferta.objects.all()[:5]
-# 		form = ContactForm()
-# 		return render(request,"contacto.html",{"ofertas":ofertas,"form": form})
 
 def contacto(request):
 	ofertas = Oferta.objects.all()[:5]
@@ -94,3 +69,29 @@ def contactoEmail(request):
 def e404(request):
 	ofertas = Oferta.objects.all()[:5]
 	return render(request,"404.html",{"ofertas":ofertas})
+
+#SIN AJAX
+# def contacto(request):
+# 	if request.method == 'POST':
+# 		form = ContactForm(request.POST)
+# 		if form.is_valid():
+# 		    name = form.cleaned_data['name']
+# 		    email = form.cleaned_data['email']
+# 		    phonenumber = form.cleaned_data['phonenumber']
+# 		    message = form.cleaned_data['message']
+
+# 		    subject = "Mensaje desde el sitio Daltonautomotriz.com"
+
+# 		    fmessage = "Nombre: " + name + "\n" + "Numero telefonico: " + phonenumber + "\n" + "Email: " + email + "\n\n" + message
+
+# 		    recipients = ['jualjiman@gmail.com']
+
+# 		    send_mail(subject, fmessage, email, recipients)
+# 		    return HttpResponseRedirect('/thanks/')
+# 		else:
+# 			ofertas = Oferta.objects.all()[:5]
+# 			return render(request,"contacto.html",{"ofertas":ofertas,"form": form})
+# 	else:
+# 		ofertas = Oferta.objects.all()[:5]
+# 		form = ContactForm()
+# 		return render(request,"contacto.html",{"ofertas":ofertas,"form": form})
