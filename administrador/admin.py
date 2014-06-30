f497700da9a95e60fa5bc834e1058d49c79f23e2
jualjiman <jualjiman@gmail.com>
2014-06-29 22:11:49 -0500

    detalles logicos en los modelos

--- administrador/admin.py
+++ administrador/admin.py
@@ -5,7 +5,7 @@ from sorl.thumbnail.shortcuts import get_thumbnail
 # Register your models here.
 
 class SliderAdmin(admin.ModelAdmin):
-	list_display = ('titulo', 'imagen_proyecto_slider')
+	list_display = ('titulo','activo', 'imagen_proyecto_slider')
 
 	def imagen_proyecto_slider(self,obj):
 		return '<img src="%s" />' % get_thumbnail(obj.imagen,'100x60', crop='center').url #format='PNG', quality=99
@@ -13,7 +13,7 @@ class SliderAdmin(admin.ModelAdmin):
 	imagen_proyecto_slider.allow_tags = True
 
 class TrabajoAdmin(admin.ModelAdmin):
-	list_display = ('titulo','descripcion', 'imagen_proyecto_trabajo')
+	list_display = ('titulo','descripcion','activo', 'imagen_proyecto_trabajo')
 
 	def imagen_proyecto_trabajo(self,obj):
 		return '<img src="%s" />' % get_thumbnail(obj.imagenPortada,'100x60', crop='center').url #format='PNG', quality=99
@@ -21,7 +21,7 @@ class TrabajoAdmin(admin.ModelAdmin):
 	imagen_proyecto_trabajo.allow_tags = True
 
 class OfertaAdmin(admin.ModelAdmin):
-	list_display = ('titulo', 'descripcion','precio', 'imagen_proyecto_oferta')
+	list_display = ('titulo', 'descripcion','precio', 'activa', 'imagen_proyecto_oferta')
 
 	def imagen_proyecto_oferta(self,obj):
 		return '<img src="%s" />' % get_thumbnail(obj.imagen,'100x60', crop='center').url #format='PNG', quality=99
@@ -29,7 +29,7 @@ class OfertaAdmin(admin.ModelAdmin):
 	imagen_proyecto_oferta.allow_tags = True
 
 class EmpleadoAdmin(admin.ModelAdmin):
-	list_display = ('nombre','cargo', 'imagen_proyecto_empleado')
+	list_display = ('nombre','cargo','activo', 'imagen_proyecto_empleado')
 
 	def imagen_proyecto_empleado(self,obj):
 		return '<img src="%s" />' % get_thumbnail(obj.imagen,'100x60', crop='center').url #format='PNG', quality=99
