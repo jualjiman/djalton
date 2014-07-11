b6a30370ddffcb74871d084d9b9726c8fcb0d84e
jualjiman <jualjiman@gmail.com>
2014-07-11 13:42:39 -0400

    mm

--- djalton/settings.py
+++ djalton/settings.py
@@ -20,9 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'x)4nmd979l7-16z*3ro4+y*=((_yc50=v&tvbt5al-aw(n^oov'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-TEMPLATE_DEBUG = False
+TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = ['.daltonautomotriz.com', '.daltonautomotriz.com.']
 
